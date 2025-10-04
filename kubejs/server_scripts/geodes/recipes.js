ac5bb7dfec41f0df82d044a6262101e76c597869
Adalynn Vivian <adalynnvivian@gmail.com>
2025-10-03 23:02:48 -0700

    Geodes recipes and tags.
    TODO: make the replacement functions not replace fluid containers or storage blocks

---
+++ kubejs/server_scripts/geodes/recipes.js
@@ -0,0 +1,76 @@
+
+/* TODO: REWRITE TO INVOLVE TAGS ONCE TINKER'S IS COMPLETE */
+ServerEvents.recipes(event => {
+    var replace = (i, o) => event.replaceInput({input: i}, i, '#' + o) //Because I know I'm going to forget the # like a dipshit
+    var replaceAll = (is, o) => is.forEach((i) => replace(i,o)) //TODO: MAKE REPLACEMENT SKIP CERTAIN RECIPES
+
+    function crystal(material, temp, time, result) {
+        event.custom({ //Small Bud
+            type: "tconstruct:melting",
+            ingredient: {
+                tag: "forge:buds/" + material + "/small"
+            },
+            time: time,
+            result: {
+                fluid: result,
+                amount: 100
+            },
+            temperature: temp
+        })
+        event.custom({ //Medium Bud
+            type: "tconstruct:melting",
+            ingredient: {
+                tag: "forge:buds/" + material + "/medium"
+            },
+            time: time*2,
+            result: {
+                fluid: result,
+                amount: 200
+            },
+            temperature: temp
+        })
+        event.custom({ //Large Bud
+            type: "tconstruct:melting",
+            ingredient: {
+                "tag": "forge:buds/" + material + "/large"
+            },
+            time: time*3,
+            result: {
+                fluid: result,
+                amount: 300
+            },
+            temperature: temp
+        })
+        event.custom({ //Cluster
+            type: "tconstruct:melting",
+            ingredient: {
+                tag: "forge:clusters/" + material
+            },
+            time: time*4,
+            result: {
+                fluid: result,
+                amount: 400
+            },
+            temperature: temp
+        })
+        event.custom({ //Crystal Block (4 gems... not 9.)
+            type: "tconstruct:melting",
+            ingredient: {
+                tag: "forge:crystal_blocks/" + material
+            },
+            time: time*2,
+            result: {
+                fluid: result,
+                amount: 400
+            },
+            temperature: temp
+        })
+    }
+    crystal("emerald", 934, 64, "tconstruct:molten_emerald")
+    crystal("quartz", 637, 55, "tconstruct:molten_quartz")
+    crystal("diamond", 1450, 79, "tconstruct:molten_diamond")
+
+    replace("geodes:gypsum_shard", "forge:gems/gypsum")
+    replace("geodes:pyrite_chunk", "forge:raw_materials/pyrite")
+    replace("geodes:pyrite", "forge:storage_blocks/gypsum")
+})
--- kubejs/server_scripts/geodes/tags.js
+++ kubejs/server_scripts/geodes/tags.js
@@ -1,9 +1,49 @@
 ServerEvents.tags('item', event => {
-
-})
-
-ServerEvents.recipes(event => {
-    function crystal(material, temp) {
-
+    function toTags(tags, item) {
+        tags.forEach(tag => {
+           event.add(tag, item)
+        })
     }
+    function toTag(tag, items) {
+        items.forEach(item => {
+            event.add(tag, item)
+        })
+    }
+    function tagItems(tags, items) {
+        tags.forEach(tag => {
+            items.forEach(item => {
+                event.add(tag, item)
+            })
+        })
+    }
+    function crystal(material, overrides) {
+        if(overrides === undefined) {
+            overrides = {}
+        }
+        var def = {
+            small: "geodes:small_" + material + "_bud",
+            medium: "geodes:medium_" + material + "_bud",
+            large: "geodes:large_" + material + "_bud",
+            cluster: "geodes:" + material + "_cluster",
+            block: "geodes:" + material + "_crystal_block"
+        }
+        for(var i in overrides) {
+            def[i] = overrides[i]
+        }
+        event.add("forge:buds/" + material + "/small", def.small)
+        event.add("forge:buds/" + material + "/medium", def.medium)
+        event.add("forge:buds/" + material + "/large", def.large)
+        event.add("forge:clusters/" + material, def.cluster)
+        event.add("forge:crystal_blocks/" + material, def.block)
+    }
+    crystal("emerald")
+    crystal("quartz")
+    crystal("diamond")
+    crystal("lapis")
+    crystal("echo", {block: "geodes:echo_block"})
+    crystal("gypsum", {cluster: "geodes:gypsum_rose"})
+
+    toTags(["forge:gems", "forge:gems/gypsum"], "geodes:gypsum_shard")
+    toTags(["forge:raw_materials", "forge:raw_materials/pyrite"], "geodes:pyrite_chunk")
+    toTags(["forge:storage_blocks", "forge:storage_blocks/raw_pyrite"], "geodes:pyrite")
 })
